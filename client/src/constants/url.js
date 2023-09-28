16e0a6d10433cb71ac3264d66a977c8098725811
Ishak Benfredj <107993434+IshakBenfredj@users.noreply.github.com>
2023-09-28 19:58:11 +0100

    Update url.js

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,4 +1,4 @@
 // const url = 'https://edutimeserver.onrender.com/' ;
 // const url = 'http://localhost:4000/' ;
-const url = 'http://193.203.165.205/' ;
+const url = 'http://193.203.165.205/api/' ;
 export default url
