6fe4d57ccf89c24e5e567f3578048ed9efc1df5a
Ishak Benfredj <107993434+IshakBenfredj@users.noreply.github.com>
2023-10-03 20:24:13 +0100

    Update url.js

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,4 +1,4 @@
 // const url = 'https://edutimeserver.onrender.com/' ;
 // const url = 'http://localhost:4000/' ;
-const url = 'https://edutime.click/api/' ;
+const url = 'https://edutime-dz.com/api/' ;
 export default url
