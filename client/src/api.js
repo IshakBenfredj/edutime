384104915ebcd8ef27083641d88953efb184b950
Ishak Benfredj <107993434+IshakBenfredj@users.noreply.github.com>
2024-08-03 19:57:15 +0100

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const Axios = axios.create({
  // baseURL: "https://edutimeserver.onrender.com",
   // baseURL: "http://localhost:8800",
-  // baseURL: "http://82.112.242.252",
+  baseURL: "http://82.112.242.252",
 });
 
 export default Axios;
